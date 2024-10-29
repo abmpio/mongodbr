8182269c6e0b9e630dff0f9c882021aca03ac4c9
jeseeqing <jeseeqing@gmail.com>
2024-10-29 10:26:31 +0800

    add Filter_Or func for builder package

--- builder/filter.go
+++ builder/filter.go
@@ -1,6 +1,17 @@
 package builder
 
+import "go.mongodb.org/mongo-driver/bson"
+
 // build filter for regex
-func Filter_Regex(filter map[string]interface{}, key string, v interface{}) {
+func Filter_Regex(filter map[string]interface{}, key string, v interface{}) map[string]interface{} {
 	filter[key] = map[string]interface{}{"$regex": v, "$options": "i"}
+	return filter
+}
+
+// 将一个key增加到or条件列表中
+func Filter_Or(filter []map[string]interface{}, key string, v interface{}) []map[string]interface{} {
+	filter = append(filter, bson.M{
+		key: v,
+	})
+	return filter
 }
