b2a14ac4a8f29207e5ccd70ddbf2e94e12fa535a
jeseeqing <jeseeqing@gmail.com>
2025-03-14 16:26:57 +0800

    feat: add FindOneOptionWithSpecifiedFields,FindOneOptionWithFieldSort that handle find one option

--- find_one_options.go
+++ find_one_options.go
@@ -1,5 +1,43 @@
 package mongodbr
 
-import "go.mongodb.org/mongo-driver/mongo/options"
+import (
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 type FindOneOption func(*options.FindOneOptions)
+
+// result with specified fields
+func FindOneOptionWithSpecifiedFields(fieldNameList []string) FindOneOption {
+	return func(fo *options.FindOneOptions) {
+		if len(fieldNameList) <= 0 {
+			return
+		}
+		projection := bson.M{}
+		for _, eachFieldName := range fieldNameList {
+			projection[eachFieldName] = 1
+		}
+		fo.SetProjection(projection)
+	}
+}
+
+func FindOneOptionWithFieldSort(fieldName string, isAsc bool) FindOneOption {
+	return func(fo *options.FindOneOptions) {
+		if fo.Sort == nil {
+			fo.Sort = bson.D{}
+		}
+		sortV, ok := fo.Sort.(bson.D)
+		if !ok {
+			return
+		}
+		sortValue := 1
+		if !isAsc {
+			sortValue = -1
+		}
+		sortV = append(sortV, bson.E{
+			Key:   fieldName,
+			Value: sortValue,
+		})
+		fo.SetSort(sortV)
+	}
+}
