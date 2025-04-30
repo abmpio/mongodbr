036b6487e9328498a951a1230e42512d86467be9
jeseeqing <jeseeqing@gmail.com>
2025-04-30 14:00:43 +0800

    feat: optimize RunTransaction func

--- transaction.go
+++ transaction.go
@@ -66,12 +66,78 @@ func newDefaultRunTransactionOptions() *RunTransactionOptions {
 	}
 }
 
-func RunTransaction[T any](fn func(mongo.SessionContext) (T, error), opts ...RunTransactionOption) (T, error) {
+func RunTransaction(fn func(mongo.SessionContext) error, opts ...RunTransactionOption) error {
 	return RunTransactionWithContext(context.Background(), fn, opts...)
 }
 
 // run fn with mongodb transaction
-func RunTransactionWithContext[T any](ctx context.Context,
+func RunTransactionWithContext(ctx context.Context,
+	fn func(mongo.SessionContext) error,
+	opts ...RunTransactionOption) error {
+
+	transactionOptions := newDefaultRunTransactionOptions()
+	for _, eachOpt := range opts {
+		eachOpt(transactionOptions)
+	}
+	maxRetry := transactionOptions.MaxRetry
+	if maxRetry <= 0 {
+		maxRetry = 3
+	}
+	// client
+	c := GetClient(transactionOptions.ClientKey)
+
+	// start session
+	session, err := c.StartSession(transactionOptions.withSessionOptions...)
+	if err != nil {
+		return err
+	}
+	defer session.EndSession(context.Background())
+
+	for attempt := 0; attempt < transactionOptions.MaxRetry; attempt++ {
+		// 每次事务执行新建一个超时ctx
+		txnCtx, cancel := context.WithTimeout(ctx, transactionOptions.Timeout)
+		defer cancel()
+
+		// perform operation
+		err = mongo.WithSession(txnCtx, session, func(sc mongo.SessionContext) error {
+			// start transaction
+			if err := session.StartTransaction(transactionOptions.withTransactionOptions...); err != nil {
+				return err
+			}
+
+			innerErr := fn(sc)
+			if innerErr != nil {
+				// roolback
+				_ = session.AbortTransaction(sc)
+				return innerErr
+			}
+			if err := sc.CommitTransaction(sc); err != nil {
+				return err
+			}
+			return nil
+		})
+
+		if err == nil {
+			return nil
+		}
+
+		// 如果是瞬时事务错误，允许重试
+		if isTransientTransactionError(err) {
+			continue
+		}
+
+		return err
+	}
+
+	return fmt.Errorf("RunTransactionWithContext: transaction failed after %d attempts, last error: %w", maxRetry, err)
+}
+
+func RunTransactionWithResult[T any](fn func(mongo.SessionContext) (T, error), opts ...RunTransactionOption) (T, error) {
+	return RunTransactionWithResultWithContext(context.Background(), fn, opts...)
+}
+
+// run fn with mongodb transaction
+func RunTransactionWithResultWithContext[T any](ctx context.Context,
 	fn func(mongo.SessionContext) (T, error),
 	opts ...RunTransactionOption) (T, error) {
 
@@ -133,7 +199,7 @@ func RunTransactionWithContext[T any](ctx context.Context,
 		return zero, err
 	}
 
-	return zero, fmt.Errorf("RunTransactionWithContext: transaction failed after %d attempts, last error: %w", maxRetry, err)
+	return zero, fmt.Errorf("RunTransactionWithResultWithContext: transaction failed after %d attempts, last error: %w", maxRetry, err)
 }
 
 // isTransientTransactionError 判断是否是瞬时事务错误
