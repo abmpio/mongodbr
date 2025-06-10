b53cd64aacef2b8635f93dd60c028a3717fdcef7
jeseeqing <jeseeqing@gmail.com>
2025-06-10 19:40:58 +0800

    feat: add AppendSetField,AppendUnsetField func in BsonBuilder structure

--- builder/bson_builder.go
+++ builder/bson_builder.go
@@ -1,6 +1,9 @@
 package builder
 
-import "go.mongodb.org/mongo-driver/bson"
+import (
+	"github.com/abmpio/libx/lang/tuple"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 const (
 	setKey   string = "$set"
@@ -19,6 +22,57 @@ func NewBsonBuilder() *BsonBuilder {
 func (b *BsonBuilder) NewOrUpdateSet(v interface{}) *BsonBuilder {
 	b.ensureBson()
 	b.bson[setKey] = v
+	return b
+}
+
+// get $set value
+// if not exist,then add $set value
+func (b *BsonBuilder) SetOrDefault() bson.M {
+	b.ensureBson()
+	setValue, ok := b.bson[setKey].(bson.M)
+	if ok && setValue != nil {
+		return setValue
+	}
+	setValue = bson.M{}
+	b.bson[setKey] = setValue
+	return setValue
+}
+
+// get $unset value
+// if not exist,then add $unset value
+func (b *BsonBuilder) UnsetOrDefault() bson.M {
+	b.ensureBson()
+	unsetValue, ok := (b.bson[unsetKey]).(bson.M)
+	if ok && unsetValue != nil {
+		return unsetValue
+	}
+	unsetValue = bson.M{}
+	b.bson[unsetKey] = unsetValue
+	return unsetValue
+}
+
+// append field value to $set field
+// V1:key V2:value
+func (b *BsonBuilder) AppendSetField(fieldValueList ...tuple.T2[string, interface{}]) *BsonBuilder {
+	if len(fieldValueList) <= 0 {
+		return b
+	}
+	setValue := b.SetOrDefault()
+	for _, eachValue := range fieldValueList {
+		setValue[eachValue.V1] = eachValue.V2
+	}
+	return b
+}
+
+// append field to $unset value
+func (b *BsonBuilder) AppendUnsetField(fieldList []string) *BsonBuilder {
+	if len(fieldList) <= 0 {
+		return b
+	}
+	unsetValue := b.UnsetOrDefault()
+	for _, eachField := range fieldList {
+		unsetValue[eachField] = ""
+	}
 	return b
 }
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/abmpio/mongodbr
 go 1.19
 
 require (
+	github.com/abmpio/libx v0.0.0-20250526021149-38a2934a3c70
 	github.com/satori/go.uuid v1.2.0
 	go.mongodb.org/mongo-driver v1.17.3
 )
@@ -16,6 +17,7 @@ require (
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
