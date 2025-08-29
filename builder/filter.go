ef9a074a307a5c90131a3a19fd7d5ebd6800b7f1
jeseeqing <jeseeqing@gmail.com>
2025-08-29 14:48:12 +0800

    feat: add Filter_RegexTo func that build regex filter

--- builder/filter.go
+++ builder/filter.go
@@ -8,6 +8,14 @@ func Filter_Regex(filter map[string]interface{}, key string, v interface{}) map[
 	return filter
 }
 
+// build filter for regex and return this filter
+func Filter_RegexTo(key string, v interface{}) map[string]interface{} {
+	filter := bson.M{
+		key: map[string]interface{}{"$regex": v, "$options": "i"},
+	}
+	return filter
+}
+
 // 将一个key增加到or条件列表中
 func Filter_Or(filter []map[string]interface{}, key string, v interface{}) []map[string]interface{} {
 	filter = append(filter, bson.M{
